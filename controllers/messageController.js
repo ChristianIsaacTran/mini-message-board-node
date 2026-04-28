c2ffe561f3aa5df1aaca8c8ff4564e112df36749
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-04-28 15:23:09 -0500

    FEAT: added form validation with express-validator, displays error message
    Implmented the basic form validation with express-validator on the
    new message form. Displays an EJS partial with include()
    if the error is detected. Note, in express validator, the result
    object returned from the validationResults() can return the errors with
    result.array(), and that makes an array of objects with the error
    info in each array element, where each error object looks like this from
    express-validator: {msg: "error message", path: "fieldPath"}

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,7 @@
 const db = require("../db/queries");
 
+const {body, validationResult, matchedData} = require("express-validator");
+
 // /index route, display all messages found in the database
 exports.renderAllMessages = async (req, res) => {
   const messages = await db.getAllMessages();
@@ -26,11 +28,25 @@ exports.renderMessageDetails = (req, res) => {
 // /new route, display new message form to user
 exports.renderNewMessageForm = (req, res) => {
   console.log("Now on new message router");
-  res.render("./newMessage/form", {});
+  res.render("./newMessage/form", {errorDetected: false});
 };
 
+
+
+// validation body (fields) check
+const authorEmptyErrMsg = "User field must not be empty.";
+const authorLengthErrMsg = "User field must be 50 characters or less";
+
+const messageEmptyErrMsg = "Message field must not be empty.";
+const messageLengthErrMsg = "Message field must be 255 characters or less";
+
+const validationChain = [
+    body("author").notEmpty().withMessage(authorEmptyErrMsg).trim().isLength({max: 50}).withMessage(authorLengthErrMsg),
+    body("message").notEmpty().withMessage(messageEmptyErrMsg).trim().isLength({max: 255}).withMessage(messageLengthErrMsg),
+];
+
 // /new route, post the new message form results to the database and redirec user to root to see
-exports.postMessageForm = async (req, res) => {
+exports.postMessageForm = [ validationChain, async (req, res) => {
 
     // add form submission to database
     // messages.push(
@@ -39,10 +55,24 @@ exports.postMessageForm = async (req, res) => {
     //     }
     // );
 
+    /*
+    check field validation, abort render POST and status 400 if 
+    validation fails.
+    */
+    const result = validationResult(req);
+
+    if(!result.isEmpty()) {
+        console.log("Validation error detected. Rendering error message.")
+        return res.status(400).render("./newMessage/form", {errorDetected: true, errorsArr: result.array()}); 
+    }
+
+    // if validation passes, get field data WITH sanitization
+    const {author, message} = matchedData(req);
+
     // add form submission to the postgreSQL database
     const formInfo = {
-        username: req.body.author,
-        text: req.body.message,
+        username: author,
+        text: message,
         date_added: new Date().toLocaleString(),
     };
 
@@ -51,4 +81,4 @@ exports.postMessageForm = async (req, res) => {
     // send users back to the index route
     res.redirect("/");
 
-};
+}];
