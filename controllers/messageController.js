0e484ae837b7bd754bb4310e2d1ccc6e78965f07
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2026-04-27 23:21:47 -0500

    FEAT: newMessageForm renders to user on link click
    Similar to the message details render, this doesn't
    query the database since it's just rendering the form
    itself, so the function is not async. Moved response
    function to messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,26 +2,32 @@ const db = require("../db/queries");
 
 // /index route, display all messages found in the database
 exports.renderAllMessages = async (req, res) => {
-    const messages = await db.getAllMessages();
+  const messages = await db.getAllMessages();
 
-    res.render("../views/index/index", {messages: messages, title: "Mini Message Board"});
+  res.render("../views/index/index", {
+    messages: messages,
+    title: "Mini Message Board",
+  });
 };
 
 // /details route, display full message from query params
 exports.renderMessageDetails = (req, res) => {
-    const user = req.query.user;
-    const text = req.query.text;
-    const added = req.query.added;
+  const user = req.query.user;
+  const text = req.query.text;
+  const added = req.query.added;
 
-    res.render("./messageDetails/messageDetails", {user: user, text: text, added: added});
+  res.render("./messageDetails/messageDetails", {
+    user: user,
+    text: text,
+    added: added,
+  });
 };
 
 // /new route, display new message form to user
 exports.renderNewMessageForm = (req, res) => {
-
+  console.log("Now on new message router");
+  res.render("./newMessage/form", {});
 };
 
 // /new route, post the new message form results to the database and redirec user to root to see
-exports.postMessageForm = async (req,res) => {
-
-};
+exports.postMessageForm = async (req, res) => {};
